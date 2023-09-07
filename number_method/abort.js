e1189940b3d1b2e7b99d6dbeee2940c8678f580a
abi <447506613@qq.com>
2023-09-07 23:48:44 +0800

    number method learning

---
+++ number_method/abort.js
@@ -0,0 +1,2 @@
+console.log(typeof 12..toFixed(2));
+console.log(12.55.toFixed(1));
---
+++ number_method/splip.js
@@ -0,0 +1,11 @@
+let num = 1000_000_000;
+console.log(num);
+console.log(typeof num);
+
+let num2 = 2e4;
+let num3 = 3e-3;
+console.log(num2);
+console.log(num3);
+
+console.log(8..toString(8));
+console.log(num.toString(32));
